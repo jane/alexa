b892bc1f3e81eb49bf9e4ba26ad45e3344ef1070
Ryan Andersen <ryan.andersen91@gmail.com>
2017-11-30 13:52:19 -0700

    Added caching to storefrontservice

--- Jane-Alexa/Jane-Alexa/Services/DealSkillService.cs
+++ Jane-Alexa/Jane-Alexa/Services/DealSkillService.cs
@@ -12,7 +12,7 @@ namespace Jane.Alexa.Services
 {
 	public interface IDealsService
     {
-        Task<SkillResponse> GetStoreFrontSpeachResponse(int take = 5, bool filterSoldOut = true, bool filterEndingSoon = true, bool filterIsNew = true);
+        Task<SkillResponse> GetStoreFrontSpeachResponse(int take = 5);
     }
 
     public class DealSkillService : IDealsService
@@ -26,12 +26,9 @@ namespace Jane.Alexa.Services
             _storeFrontService = storeFrontService;
         }
 
-        public async Task<SkillResponse> GetStoreFrontSpeachResponse(int take = 5,
-            bool filterSoldOut = true,
-            bool filterEndingSoon = true,
-            bool filterIsNew = true)
+        public async Task<SkillResponse> GetStoreFrontSpeachResponse(int take = 5)
         {
-            var storeFrontItems = await _storeFrontService.GetStorefront(take, filterSoldOut, filterEndingSoon, filterIsNew)
+            var storeFrontItems = await _storeFrontService.GetStorefront(take)
                 .ConfigureAwait(false);
             var speechResponse = new SsmlOutputSpeech();
 
